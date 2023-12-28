51866ca2f601f0615efa411e7c2128574652faf8
Tomás Lungenstrass <tomas.lungenstrass@kwali.ai>
2023-12-28 12:54:24 -0300

    initial commit

---
+++ geno-whirl/mp3sort/__init__.py
@@ -0,0 +1,13 @@
+# ===============================
+# mp3sort
+# ===============================
+# Tomás Lungenstrass, 2023
+"""
+Allows to sort an mp3 library into a given folder structure by metadata values.
+
+Routine Listing
+---------------
+
+"""
+
+__version__ = "0.0.1"
